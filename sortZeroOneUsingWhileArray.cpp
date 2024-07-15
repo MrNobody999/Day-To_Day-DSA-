a61e9f25c30fdc289a9a159d2d6b9caa03621d97
Rushabh_Kalukhe799 <87063738+MrNobody999@users.noreply.github.com>
2024-07-15 15:30:58 +0530

    Create sortZeroOneUsingWhileArray.cpp

---
+++ sortZeroOneUsingWhileArray.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+void sortZeroOnes(int arr[], int size) {
+  int countZero = 0;
+  int countOne = 0;
+
+  for (int i = 0; i < size; i++) {
+    if (arr[i] == 0) {
+      countZero++;
+    }
+    if (arr[i] == 1) {
+      countOne++;
+    }
+  }
+
+  int index = 0;
+  while (countZero--) {
+    arr[index] = 0;
+    index++;
+  }
+  while (countOne--) {
+    arr[index] = 1;
+    index++;
+  }
+}
+
+int main() {
+  int arr[] = {0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1};
+  int size = 15; // 1-6  0-9
+
+  sortZeroOnes(arr, size);
+
+  for (int i = 0; i < size; i++) {
+    cout << arr[i] << " ";
+  }
+
+  return 0;
+}
