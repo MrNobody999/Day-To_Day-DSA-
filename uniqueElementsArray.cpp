25e6fe27a721f9857da199cdaf3a1fb1b0b2693a
Rushabh_Kalukhe799 <87063738+MrNobody999@users.noreply.github.com>
2024-07-15 10:54:08 +0530

    Create uniqueElementsArray.cpp
    This program gives unique element in array.

---
+++ uniqueElementsArray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int uniqueElementArray(int arr[], int size) {
+  int ans = 0;
+  for (int i = 0; i < size; i++) {
+    ans = ans ^ arr[i];
+  }
+  return ans;
+}
+
+int main() {
+  int arr[] = {2, 10, 11, 13, 10, 2, 15, 13, 15};
+  int size = 9;
+
+  int finalAns = uniqueElementArray(arr, size);
+  cout << "Unique element in array is: " << finalAns << endl;
+
+  return 0;
+}
