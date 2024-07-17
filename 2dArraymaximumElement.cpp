3b0fdd57cff632c5a5b228dc2e78d51da396f8ce
Rushabh_Kalukhe799 <87063738+MrNobody999@users.noreply.github.com>
2024-07-17 11:35:43 +0530

    Create 2dArraymaximumElement.cpp
    This program finds maximum element in 2-d array.

---
+++ 2dArraymaximumElement.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int maximumElement(int arr[][3], int row, int col, int target) {
+  int maxi= INT_MIN;
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      if (arr[i][j] >= maxi) {
+        maxi = arr[i][j];
+      }
+    }
+  }
+  return maxi;
+}
+
+int main() {
+
+  int arr[3][3] = {{10, 20, 30}, {40, 500, 60}, {70, 80, 90}};
+
+  int row = 3;
+  int col = 3;
+
+  int target = 90;
+
+  int result = maximumElement(arr, row, col, target);
+
+  cout << "Maximum element in 2-d array is: "<< result << endl;
+  
+  return 0;
+}
