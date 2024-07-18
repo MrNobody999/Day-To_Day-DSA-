9d693d3597daa8591356426b584008f39daa6ca9
Rushabh_Kalukhe799 <87063738+MrNobody999@users.noreply.github.com>
2024-07-18 11:52:17 +0530

    Create 2-dArrayTranposeMatrix.cpp

---
+++ 2-dArrayTranposeMatrix.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+void transposeMatrix(int arr[][4], int row, int col) {
+  for (int i = 0; i < row; i++) {
+    for (int j = i + 1; j < col; j++) {
+      if (i == j) {
+        continue;
+      } else {
+        swap(arr[i][j], arr[j][i]);
+      }
+    }
+  }
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      cout << arr[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
+int main() {
+
+  int arr[4][4] = {
+      {3, 4, 7, 8}, {11, 5, 17, 22}, {2, 12, 23, 9}, {10, 16, 21, 6}};
+
+  int row = 4;
+  int col = 4;
+
+  transposeMatrix(arr, row, col);
+
+  return 0;
+}
