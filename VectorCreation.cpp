09e9920c5cde62077a67d273a60bcb20c6b9aa53
Rushabh_Kalukhe799 <87063738+MrNobody999@users.noreply.github.com>
2024-07-20 23:31:40 +0530

    Update VectorCreation.cpp

--- VectorCreation.cpp
+++ VectorCreation.cpp
@@ -13,13 +13,7 @@ using namespace std;
 //       }
 //     }
 //   }
-//   for (int i = 0; i < row; i++) {
-//     for (int j = 0; j < col; j++) {
-//       cout << arr[i][j] << " ";
-//     }
-//     cout << endl;
-//   }
-// }
+// 
 
 int main() {
 
