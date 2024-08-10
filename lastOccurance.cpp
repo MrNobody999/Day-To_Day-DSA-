07ae8db7d9af146bc22d9be26649824cfcce8989
Rushabh_Kalukhe799 <87063738+MrNobody999@users.noreply.github.com>
2024-08-10 16:51:59 +0530

    Create lastOccurance.cpp

---
+++ lastOccurance.cpp
@@ -0,0 +1,43 @@
+// Question : Find first occurance of element in array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int lastOccurance(vector<int> arr, int n, int target) {
+  int start = 0;
+  int end = n - 1;
+  int mid = (start + end) / 2;
+  int ans = -1;
+
+  while (start <= end) {
+    if (arr[mid] == target) {
+      ans = mid;
+      start = mid + 1;
+    } else if (target > arr[mid]) {
+      start = mid + 1;
+    } else if (target < arr[mid]) {
+      end = mid - 1;
+    }
+    mid = (start + end) / 2; // Bhul jate hai hum ye
+  }
+
+  return ans;
+}
+
+int main() {
+
+  vector<int> arr = {10, 20, 30, 30, 30, 30, 30, 50};
+  int target = 30;
+  int n = arr.size();
+
+  int ans = lastOccurance(arr, n, target);
+
+  if (ans == -1) {
+    cout << "Element not found";
+  } else {
+    cout << "Last occurance of " << target << " found at index : " << ans
+         << endl;
+  }
+
+  return 0;
+}
